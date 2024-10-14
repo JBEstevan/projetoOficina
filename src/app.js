7dedfd6f8bbb774c57bad3c8b99aeeb0def983a6
JBEstevan <jbe.estevan@gmail.com>
2024-10-14 20:08:28 -0300

    feat: add .env para deploy

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import "dotenv/config";
 import workshopRouter from './routers/workshopRouter.js';
 import vehicleRouter from './routers/vehicleRouter.js';
 import maintenanceRouter from './routers/maintenanceRouter.js';
-import dbConfig from './config/dbConfig.js';
-
-dbConfig();
+/* import dbConfig from './config/dbConfig.js'; */
 
 const app = express();
 
@@ -14,4 +13,4 @@ app.use('/workshop', workshopRouter);
 app.use('/vehicle', vehicleRouter);
 app.use('/maintenance', maintenanceRouter);
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(10000, () => console.log('Server is running on port 10000'));
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,10 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-function dbConfig() {
-    mongoose
-    .connect('mongodb://localhost:27017/workshop')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.log(err));
-}
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
+            // `mongoose.connect('mongodb+srv://<DB_USER>:<PASSWORD>i@<CLUSTER_NAME>/<COLLECTION_NAME>');`
+        );
 
-export default dbConfig;
+    } catch (error) {
+        console.error('Erro ao conectar ao MongoDB', error);
+
+    }
+};
+
+connectDB();
+
+export default mongoose;
